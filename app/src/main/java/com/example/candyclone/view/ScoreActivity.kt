00b73f71f8416fd3dd468fe8cdd39bfd2eb9c51a
Magoulo <83534780+Magoulo@users.noreply.github.com>
2022-07-30 19:20:06 +0200

    created activity and layoutfile for final score

---
+++ app/src/main/java/com/example/candyclone/view/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.candyclone.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.candyclone.R
+
+class ScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
